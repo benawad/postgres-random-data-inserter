d8a08a95fdc5256196bc04dc8f677e9d2547c694
Ben Awad <benawad97@gmail.com>
2020-01-12 14:38:44 -0600

    kill dupes for join table

--- utils/columnsToFakeInserts.ts
+++ utils/columnsToFakeInserts.ts
@@ -3,7 +3,7 @@ import faker from "faker";
 
 const colToFakeValue = (c: ColumnInfo) => {
   if (c.dataType === "int" || c.dataType === "integer") {
-    return Math.floor(Math.random() * 100) + 1;
+    return faker.random.number({ min: 1, max: 100 });
   }
 
   if (c.dataType === "text" || c.dataType === "varchar") {
@@ -34,12 +34,24 @@ export const columnsToFakeInserts = (table: Table, seed: number) => {
   }
 
   const inserts: string[] = [];
+  const isJoinTable = table.columns.every(x => !!x.ref);
+  const history: Record<string, boolean> = {};
 
   for (let i = 0; i < 100; i++) {
+    let v = table.columns.map(colToFakeValue).join(",");
+    if (isJoinTable) {
+      let maxIters = 100;
+      while (v in history && maxIters) {
+        maxIters--;
+        v = table.columns.map(colToFakeValue).join(",");
+      }
+      history[v] = true;
+    }
+
     inserts.push(
       `insert into ${table.name} (${table.columns
         .map(c => c.name)
-        .join(",")}) values (${table.columns.map(colToFakeValue).join(",")});`
+        .join(",")}) values (${v});`
     );
   }
 
